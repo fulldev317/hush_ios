d6e928e4f3bfacfbcfb0729a9b8514f4dde8c7e3
Hasan Musaj <hasan.musaj312@protonmail.com>
2020-05-04 14:42:01 +0300

    Add .gitignore

---
+++ Hush-SwiftUI/Hush-SwiftUI/Modules/Auth/NewFaceDetection/NewFaceDetection.swift
@@ -0,0 +1,21 @@
+//
+//  NewFaceDetection.swift
+//  Hush-SwiftUI
+//
+//  Created by Serge Vysotsky on 04.05.2020.
+//  Copyright © 2020 AppServices. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewFaceDetection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewFaceDetection_Previews: PreviewProvider {
+    static var previews: some View {
+        NewFaceDetection()
+    }
+}
