b847d2e1934132df1e746ab01bca91f1b6851a36
Hasan Musaj <hasan.musaj312@protonmail.com>
2020-05-08 16:17:08 +0300

    Two thumb Slider

--- Hush-SwiftUI/Hush-SwiftUI/Helpers/Preferences/Size.swift
+++ Hush-SwiftUI/Hush-SwiftUI/Helpers/Preferences/Size.swift
@@ -9,10 +9,9 @@
 import SwiftUI
 
 struct SizeKey: PreferenceKey {
-    static var defaultValue = CGSize.zero
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
-        value = CGRect(origin: .zero, size: value)
-            .union(CGRect(origin: .zero, size: nextValue())).size
+    static var defaultValue: CGSize?
+    static func reduce(value: inout CGSize?, nextValue: () -> CGSize?) {
+        value = value ?? nextValue()
     }
 }
 
@@ -21,9 +20,9 @@ struct SizeObserver: ViewModifier {
     
     func body(content: Content) -> some View {
         content.background(GeometryReader { proxy in
-            EmptyView().preference(key: SizeKey.self, value: proxy.size)
+            Color.clear.preference(key: SizeKey.self, value: proxy.size)
         }).onPreferenceChange(SizeKey.self) { size in
-            self.size = size
+            self.size = size ?? .zero
         }
     }
 }
