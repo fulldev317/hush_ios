ce4ecb4ee1cf228546cc389f5310e308218a5df1
Hasan Musaj <hasan.musaj312@protonmail.com>
2020-05-14 01:34:36 +0300

    Fix dark mode issue

--- Hush-SwiftUI/Hush-SwiftUI/App/AppDelegate.swift
+++ Hush-SwiftUI/Hush-SwiftUI/App/AppDelegate.swift
@@ -12,7 +12,9 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        UIApplication.shared.windows.forEach { window in
+            window.overrideUserInterfaceStyle = .light
+        }
         return true
     }
 
