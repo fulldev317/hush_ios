4e40464ae6d15c498f94eeea69c8aec61ee8dd11
fulldev317 <fulldev317@gmail.com>
2020-06-22 10:37:27 +0200

    Add Splash Page

--- Hush-SwiftUI/Hush-SwiftUI/App/SceneDelegate.swift
+++ Hush-SwiftUI/Hush-SwiftUI/App/SceneDelegate.swift
@@ -107,15 +107,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                         .environmentObject(PartialSheetManager())
                         .environmentObject(self.app)
                     )
+                } else {
+                    self.window?.rootViewController = UIHostingController(rootView:
+                        NavigationView {
+                            SplashView()
+                                .hostModalPresenter()
+                                .edgesIgnoringSafeArea(.all)
+                                .withoutBar()
+                        }
+                        .environmentObject(PartialSheetManager())
+                        .environmentObject(self.app)
+                    )
                 }
             } else {
                 self.window?.rootViewController = UIHostingController(rootView:
                     NavigationView {
-                        //SignUpView(viewModel: SignUpViewModel()).withoutBar()
+                        SignUpView(viewModel: SignUpViewModel()).withoutBar()
                         //SignUpEmail(viewModel: SignUpEmailViewModel()).withoutBar()
                         //LoginView(viewModel: LoginViewModel()).withoutBar()
                         //LoginWithEmailView(viewModel: LoginWithEmailViewModel()).withoutBar()
-                        GetMoreDetailsView(viewModel:       GetMoreDetailsViewModel(name: "Maksym", username: "max", email: "max@gmail.com", password: "111111", image: UIImage())).withoutBar()
+                        //GetMoreDetailsView(viewModel:       GetMoreDetailsViewModel(name: "Maksym", username: "max", email: "max@gmail.com", password: "111111", image: UIImage())).withoutBar()
                         //AddPhotosView(viewModel: AddPhotosViewModel(name: "Maksym", username: "max3", email: "max3@gmail.com", password: "123456")).withoutBar()
                         
                     }
