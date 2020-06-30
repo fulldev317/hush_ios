d76cc56789296a062ca25ca1072328746cd028b0
fulldev317 <fulldev317@gmail.com>
2020-06-30 21:13:50 +0200

    Fixed the indicator issue in Looking Good page

--- Hush-SwiftUI/Hush-SwiftUI/App/SceneDelegate.swift
+++ Hush-SwiftUI/Hush-SwiftUI/App/SceneDelegate.swift
@@ -125,12 +125,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             } else {
                 self.window?.rootViewController = UIHostingController(rootView:
                     NavigationView {
-                        //SignUpView(viewModel: SignUpViewModel()).withoutBar()
+                        SignUpView(viewModel: SignUpViewModel()).withoutBar()
                         //SignUpEmail(viewModel: SignUpEmailViewModel()).withoutBar()
                         //LoginView(viewModel: LoginViewModel()).withoutBar()
                         //LoginWithEmailView(viewModel: LoginWithEmailViewModel()).withoutBar()
                         //GetMoreDetailsView(viewModel:       GetMoreDetailsViewModel(name: "Maksym", username: "max4", email: "max4@gmail.com", password: "111111", image: UIImage(), imagePath: "", imageThumb: "")).withoutBar()
-                        AddPhotosView(viewModel: AddPhotosViewModel(name: "Maksym", username: "max3", email: "max3@gmail.com", password: "123456")).withoutBar()
+                        //AddPhotosView(viewModel: AddPhotosViewModel(name: "Maksym", username: "max3", email: "max3@gmail.com", password: "123456")).withoutBar()
                         
                     }
                     .environmentObject(PartialSheetManager())
