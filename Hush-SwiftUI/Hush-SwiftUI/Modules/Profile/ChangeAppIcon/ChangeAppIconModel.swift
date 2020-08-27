5fe63a44a151c87a044e8d68790c120fc09ff4bc
fulldev317 <fulldev317@gmail.com>
2020-08-27 16:44:46 +0200

    Added ChangeAppIcon page

---
+++ Hush-SwiftUI/Hush-SwiftUI/Modules/Profile/ChangeAppIcon/ChangeAppIconModel.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeAppIconModel.swift
+//  Hush-SwiftUI
+//
+//  Created by fulldev on 8/27/20.
+//  Copyright © 2020 AppServices. All rights reserved.
+//
+
+import Foundation
---
+++ Hush-SwiftUI/Hush-SwiftUI/Modules/Profile/ChangeAppIcon/ChangeAppIconModeled.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeAppIconModeled.swift
+//  Hush-SwiftUI
+//
+//  Created by fulldev on 8/27/20.
+//  Copyright © 2020 AppServices. All rights reserved.
+//
+
+import Foundation
